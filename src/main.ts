878f9a953f17af6eb7567aee0e2e00edc7b585cc
Caleb Adepitan <calebpitan@outlook.com>
2021-07-05 09:27:20 +0100

    Use PORT from env var

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { Configuration } from './utils/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const appConfig = app
+    .get<ConfigService<Configuration>>(ConfigService)
+    .get<Configuration['app']>('app')
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
-  await app.listen(3000)
+  await app.listen(appConfig!.port)
 }
 bootstrap()
